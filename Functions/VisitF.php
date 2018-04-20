41dd4d0157115e8350e27c47bac279ee58f8c282
lyndon bowen <lyndonbowen33@gmail.com>
2018-04-20 19:39:53 +0000

    Second Push

--- Functions/VisitF.php
+++ Functions/VisitF.php
@@ -1,20 +1,54 @@
 <?php
+
+$submit = $_POST['submit'];
+
+function visitI(){
+    
 include "conn.php";
 
 $FName = $_POST['FirstName'];
 $LName = $_POST['LastName'];
 $Organization = $_POST['Organization'];
 $Reason = $_POST['Reason'];
-$Duration = $_POST['Duration'];
 
 date_default_timezone_set("America/New_York");
-$add = strtotime('+' . $Duration .' hour');
 
 $TimeIn = date('Y-m-d H:i:s');
-$TimeOut = date('Y-m-d H:i:s', $add);
 
-$push = mysqli_query($connect, "insert into Visit (FName,LName,TimeIn,TimeOut,Organization,VReason) values ('$FName','$LName','$TimeIn','$TimeOut','$Organization','$Reason')");
+
+$push = mysqli_query($connect, "insert into Visit (FName,LName,TimeIn,TimeOut,Organization,VReason) values ('$FName','$LName','$TimeIn','0000-00-00 00:00:00','$Organization','$Reason')");
 
 header('Location: ../index.php');
+
+}
+
+function visitO(){
+  
+  include "conn.php";
+  
+  date_default_timezone_set("America/New_York");
+  $TimeOut = date('Y-m-d H:i:s');
+  $FName = $_POST['FirstName'];
+  $LName = $_POST['LastName'];
+  $Organization = $_POST['Organization'];
+  
+  $update = mysqli_query($connect,"UPDATE Visit SET TimeOut = '$TimeOut' WHERE LName = '$LName' and FName ='$FName' and Organization = '$Organization' ");
+    
+  header('Location: ../index.php');
+    
+    
+}
+
+
+
+
+
+switch($submit){
+    
+case 1: visitI();
+    break;
+case 2: visitO();
+    break;
+}
 
 ?>
--- Pages/Visitor.php
+++ Pages/Visitor.php
@@ -1,10 +1,10 @@
 <html> 
 
 <head>
-    
- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" integrity="sha384-feJI7QwhOS+hwpX2zkaeJQjeiwlhOP+SdQDqhgvvo1DsjtiSQByFdThsxO669S2D" crossorigin="anonymous"></script>   
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     
 </head>
 
@@ -19,7 +19,7 @@
         
         <div style="margin-left:auto; margin-right:auto; width:50%; margin-top:90px;">
            
-           <h1 style="text-align:center;">Visitor Sign In</h1> <br>
+           <h1 style="text-align:center;">Visitor Sign In/Out</h1> <br>
               <form  action="../Functions/VisitF.php" method="post"> 
               
 					
@@ -33,17 +33,96 @@
 					
 					
 					<input class="form-control" placeholder="Reason" type="text" Name="Reason" value=""><br>
-					     
-					
-					<input class="form-control" placeholder="Duration" type="text" Name="Duration" value=""><br>
-					
+				
               
-              
-              
-              
-                  <button class="form-control btn btn-primary" type="submit">Visit</button>
+                  <button class="form-control btn btn-primary" type="submit" name="submit" value="1">Begin Visit</button>
        
               </form> 
+              
+                <button class="form-control btn btn-danger" type="submit" data-toggle='modal' data-target='#exampleModal2'>Finish Visit</button>
+                
+              
+              <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel2">Check Out</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                       <form  action="../Functions/VisitF.php" method="post"> 
+                      
+                        
+                        <select class="form-control" name="FirstName" required>
+                            <option>Select Your First Name</option>
+                            <?php
+                                
+                                  include "../Functions/conn.php";
+                                                                
+                                  $query = mysqli_query($connect,"select * from Visit where TimeOut = '0000-00-00 00:00:00'");
+                                  $get = mysqli_fetch_all($query,MYSQLI_ASSOC);
+                                  print_r($get);                
+                                  foreach( $get as $get){
+                                        echo "<option>" . $get['FName'] . "</option>";
+                                  }
+                                                                
+                            
+                            ?>
+                            
+                        </select><br/>
+                        
+                        <select class="form-control" name="LastName" required>
+                             <option>Select Your Last Name</option>
+                            <?php
+                                
+                                  include "../Functions/conn.php";
+                                                                
+                                  $query = mysqli_query($connect,"select * from Visit where TimeOut = '0000-00-00 00:00:00'");
+                                  $get = mysqli_fetch_all($query,MYSQLI_ASSOC);
+                                  print_r($get);                
+                                  foreach( $get as $get){
+                                        echo "<option>" . $get['LName'] . "</option>";
+                                  }
+                                                                
+                            
+                            ?>
+                            
+                        </select><br/>
+                        
+                        <select class="form-control" name="Organization" required>
+                             <option>Select Your Organization Name</option>
+                            <?php
+                                
+                                  include "../Functions/conn.php";
+                                                                
+                                  $query = mysqli_query($connect,"select * from Visit where TimeOut = '0000-00-00 00:00:00'");
+                                  $get = mysqli_fetch_all($query,MYSQLI_ASSOC);
+                                  print_r($get);                
+                                  foreach( $get as $get){
+                                        echo "<option>" . $get['Organization'] . "</option>";
+                                  }
+                                                                
+                            
+                            ?>
+                            
+                        </select><br/>
+                        
+                  </div>
+                  <div class="modal-footer">
+                   
+                        <button class="form-control btn btn-danger" name="submit" type="submit" value="2">Checkout</button>
+                    
+                    </form>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>  
+
+
+   </div> 
               
           </div> 
           
